9a26178944fedf4d948d8651c555fc62a7a9ef71
Jackson Achemu <achemuj@gmail.com>
2021-12-04 15:13:01 -0600

    completed the project screens and functionalities

--- client/blogmega/src/Components/NavBar/NavBar.jsx
+++ client/blogmega/src/Components/NavBar/NavBar.jsx
@@ -1,79 +1,110 @@
-import "./NavBar.css";
-import { Link } from "react-router-dom";
+import './NavBar.css';
+import { Link } from 'react-router-dom';
+import { useContext, useRef, useState } from 'react';
+import { Context } from '../../Context/Context';
+import axios from 'axios';
 
 export default function NavBar() {
-	const user = false;
-	return (
-		<div className="nav">
-			<div className="navLeft">
-				{user ? (
-					<img
-						className="navImage"
-						src="https://pokecharms.com/data/attachment-files/2015/10/236932_Bulbasaur_Picture.png"
-						alt="profilePicture"
-						srcset=""
-					/>
-				) : (
-					<ul className="navList">
-						<li className="navListItem">
-							<Link className="link" to="/login">
-								LOGIN
-							</Link>
-						</li>
-						<li className="navListItem">
-							<Link className="link" to="/register">
-								REGISTER
-							</Link>
-						</li>
-					</ul>
-				)}
-			</div>
-			<div className="navCenter">
-				<ul className="navList">
-					<li className="navListItem">
-						<Link to="/" className="link">
-							HOME
-						</Link>
-					</li>
-					<li className="navListItem">
-						<Link to="/posts" className="link">
-							POSTS
-						</Link>
-					</li>
-					<li className="navListItem">
-						<Link to="/about" className="link">
-							ABOUT
-						</Link>
-					</li>
-					{user ? (
-						<li className="navListItem">
-							<Link to="/create-post" className="link">
-								WRITE
-							</Link>
-						</li>
-					) : (
-						""
-					)}
-					<li className="navListItem">
-						<Link to="/logout" className="link">
-							{user && "LOGOUT"}
-						</Link>
-					</li>
-				</ul>
-			</div>
-			<div className="navRight">
-				<i className="navIcon fab fa-facebook-f"></i>
-				<i className="navIcon fab fa-instagram"></i>
-				<i className="navIcon fab fa-linkedin-in"></i>
-				<i className="navIcon fab fa-twitter"></i>
-				<div className="navSearchBar navListItem">
-					<input
-						className="searchInput"
-						type="text"
-						placeholder="Search"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+    const { user, dispatch } = useContext(Context);
+
+    const [search, setSearch] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (search.length > 0) {
+            console.log(search.charAt(0).toUpperCase() + search.slice(1));
+            window.location.replace('/posts/?category=' + search.charAt(0).toUpperCase() + search.slice(1));
+        }
+    };
+
+    const handleLogout = () => {
+        dispatch({ type: 'LOGOUT' });
+    };
+
+    return (
+        <div className='nav'>
+            <div className='navLeft'>
+                {user ? (
+                    <img
+                        className='navImage'
+                        src={
+                            user
+                                ? user.displayPicture
+                                : 'https://i.pinimg.com/564x/04/5c/53/045c539f0a84487c87bc3c59b6647aac.jpg'
+                        }
+                        alt='profilePicture'
+                    />
+                ) : (
+                    <ul className='navList'>
+                        <li className='navListItem'>
+                            <Link className='link' to='/login'>
+                                LOGIN
+                            </Link>
+                        </li>
+                        <li className='navListItem'>
+                            <Link className='link' to='/register'>
+                                REGISTER
+                            </Link>
+                        </li>
+                    </ul>
+                )}
+            </div>
+            <div className='navCenter'>
+                <ul className='navList'>
+                    <li className='navListItem'>
+                        <Link to='/' className='link'>
+                            HOME
+                        </Link>
+                    </li>
+                    <li className='navListItem'>
+                        <Link to='/posts' className='link'>
+                            POSTS
+                        </Link>
+                    </li>
+                    <li className='navListItem'>
+                        <Link to='/profile' className='link'>
+                            PROFILE
+                        </Link>
+                    </li>
+                    {user ? (
+                        <li className='navListItem'>
+                            <Link to='/create-post' className='link'>
+                                WRITE
+                            </Link>
+                        </li>
+                    ) : (
+                        ''
+                    )}
+                    <li className='navListItem' onClick={handleLogout}>
+                        {user && 'LOGOUT'}
+                    </li>
+                </ul>
+            </div>
+            <div className='navRight'>
+                <a href='https://www.facebook.com/achemu.jackson' target='_blank' rel='noreferrer'>
+                    <i className='navIcon fab fa-facebook-f'></i>
+                </a>
+                <a href='https://www.instagram.com/jachemu/' target='_blank' rel='noreferrer'>
+                    <i className='navIcon fab fa-instagram'></i>
+                </a>
+                <a href='https://www.linkedin.com/in/afcamlogistics/' target='_blank' rel='noreferrer'>
+                    <i className='navIcon fab fa-linkedin-in'></i>
+                </a>
+                <a href='https://twitter.com/JAchemu' target='_blank' rel='noreferrer'>
+                    <i className='navIcon fab fa-twitter'></i>
+                </a>
+                <form className='navSearchBar navListItem' onSubmit={handleSubmit}>
+                    <input
+                        className='searchInput'
+                        type='text'
+                        onChange={(e) => setSearch(e.target.value)}
+                        placeholder='Search posts by category'
+                    />
+                    <button className='search' type='submit'>
+                        <i class='iconSearch fas fa-search'></i>
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,13 @@
 const AppError = require('../utils/handle.errors');
 const authService = require('../services/auth.service');
+const uuid4 = require('uuid4');
+const { cloudinary } = require('../utils/handeFileUpload');
 
 class AuthController {
     constructor() {}
-    async signUp(req, res, next) {
-        const result = await authService.signUp(req.body, next);
-        res.status(result.statusCode).json({
+    async signUp(req, res) {
+        const result = await authService.signUp(req.body);
+        return res.status(result.statusCode).json({
             status: result.status,
             user: result.user,
             message: result.message,
@@ -15,7 +17,7 @@ class AuthController {
 
     async login(req, res) {
         const result = await authService.login(req.body);
-        return res.status(result.statusCode).send({
+        return res.status(result.statusCode).json({
             status: result.status,
             message: result.message,
             user: result.user,
@@ -26,6 +28,22 @@ class AuthController {
     async logOut(req, res) {
         const id = req.params.id;
         // TODO: Delete the token from the cookie or set the token to expire in 1sec
+    }
+
+    async updateUser(req, res) {
+        const { url } = await cloudinary.uploader.upload(req.file.path, {
+            public_id: uuid4() + req.file.originalname,
+            width: 300,
+            height: 300,
+            crop: 'fill',
+        });
+        req.body.displayPicture = url;
+        const result = await authService.updateUser(req.body);
+        return res.status(result.statusCode).json({
+            status: result.status,
+            message: result.message,
+            user: result.user,
+        });
     }
 }
 module.exports = new AuthController();
